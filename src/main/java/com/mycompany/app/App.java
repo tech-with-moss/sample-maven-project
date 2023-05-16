c54b837ebc090808c3b11beb04b9630d9d78b34d
irfan-1117 <52486859+irfan-1117@users.noreply.github.com>
2023-05-16 12:05:10 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello this is something to do" );
+        System.out.println( "Hello this is something to learn" );
     }
 }
