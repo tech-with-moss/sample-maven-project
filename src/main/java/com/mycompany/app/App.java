ba6b88a8b856b18423ddaaee38739e536e4080bc
irfan-1117 <52486859+irfan-1117@users.noreply.github.com>
2023-05-16 16:13:11 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello world deveops tutorials" )
+        System.out.println( "Hello world deveops tutorials" );
     }
 }
